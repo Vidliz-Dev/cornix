a8925b9eb8ed45c1b2842dc579be404761b3e1e1
Alexey-Geyvan <alexey.geyvan@kawalee.com>
2022-01-12 18:14:17 +0200

    all channels

---
+++ block-templates/supported-channels/all-channels/template.php
@@ -0,0 +1,79 @@
+<?php 
+// blank--title
+// available fields
+[
+'title'           => $title,
+'some_class'      => $some_class,
+'some_id'         => $some_id,
+'inner'           => $inner,
+'template_path'   => $template_path,
+'template_id'     => $template_id,
+] = $args;
+?>
+
+<?php 
+// wp_enqueue_script("script-id");  // load previously registered script
+?>
+
+
+
+<section class="all-channels block pt-50">    
+    <div class="container">
+
+        <div class="row">
+            <div class="col-12">
+                <ul class="nav">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-expanded="false">Exchanges</a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">Exchange1</a>
+                            <a class="dropdown-item" href="#">Exchange2</a>
+                            <a class="dropdown-item" href="#">Exchange3</a>                           
+                            <a class="dropdown-item" href="#">Exchange4</a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-expanded="false">Spot/Margin</a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">All Exchanges</a>    
+                            <a class="dropdown-item" href="#">Spot Exchanges</a>
+                            <a class="dropdown-item" href="#">Margin Exchanges</a>                            
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-expanded="false">Free/Paid</a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">All Signals</a>    
+                            <a class="dropdown-item" href="#">Free Signals</a>
+                            <a class="dropdown-item" href="#">Paid Signals</a>                            
+                        </div>
+                    </li>                   
+                </ul>
+            </div>
+        </div>
+
+        <div class="row">
+            <?php
+            $query = new WP_Query( array(
+                'post_type' => 'channels',   
+                'posts_per_page' => -1   
+            ) );
+            while ( $query->have_posts() ) : $query->the_post();
+            ?>
+                <div class="col-12 col-sm-6 col-lg-4 col-xl-3" data-exchange="" data-spot="" data-signal="">
+                    <div class="card channel py-40 px-40 text-center" onclick="window.location.href='<?php the_field('link'); ?>'">
+                        <img class="channel__logo mb-40" src="<?php the_field('logo'); ?>" alt="<?php the_title; ?>">
+                        <h3 class="channel__title"><?php the_title; ?></h3>
+                    </div>
+                </div>   
+            <?php
+            endwhile;				
+            wp_reset_query();
+            ?>                     
+        </div>
+
+    </div>
+</section>
+
+
+
---
+++ block-templates/supported-channels/firstscreen/template.php
@@ -0,0 +1,41 @@
+<?php 
+// blank--title-subtitle
+// available fields
+[
+'title'           => $title,
+'subtitle'        => $subtitle,
+'some_class'      => $some_class,
+'some_id'         => $some_id,
+'inner'           => $inner,
+'template_path'   => $template_path,
+'template_id'     => $template_id,
+] = $args;
+?>
+
+<?php 
+// wp_enqueue_script("script-id");  // load previously registered script
+// wp_enqueue_style('style-id'); // load some other registered styles
+?>
+
+
+
+<section class="channels-firstscreen block pb-0">    
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="section-title mb-25">
+                    <h2><?php echo $title; ?></h2>                    
+                </div> 
+                <p class="mb-40 pb-10" data-aos="fade-in">
+                    <?php echo $subtitle; ?>
+                </p>
+                <p class="font-weight-bold">
+                    <?php echo $inner; ?>
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
