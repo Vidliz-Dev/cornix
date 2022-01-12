5474e407d5c476518e3a9ede7f4ae06b8296c26c
Alexey-Geyvan <alexey.geyvan@kawalee.com>
2022-01-12 18:39:02 +0200

    Update template.php

--- block-templates/supported-channels/all-channels/template.php
+++ block-templates/supported-channels/all-channels/template.php
@@ -62,7 +62,7 @@
             while ( $query->have_posts() ) : $query->the_post();
             ?>
                 <div class="col-12 col-sm-6 col-lg-4 col-xl-3 mb-25" data-exchange="" data-spot="" data-signal="">
-                    <div class="card channel py-40 px-40 text-center" onclick="window.location.href='<?php the_field('link'); ?>'">
+                    <div class="card channel py-40 px-40 text-center d-flex flex-column align-items-center" onclick="window.location.href='<?php the_field('link'); ?>'">
                         <img class="channel-logo mb-40" src="<?php the_field('logo'); ?>" alt="<?php the_title(); ?>" style="width: 120px; height:120px;">
                         <h3 class="card-title mb-0"><?php the_title(); ?></h3>
                     </div>
